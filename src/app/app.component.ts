86a487b2a52244a2b4c9efb682be9d7ece6d302e
Tyler Hodzen <38892841+thodzen@users.noreply.github.com>
2020-08-05 13:48:37 -0400

    after lunch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'few100ng';
+  title = 'Front End Web 100 - The Beginning!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TipCalculatorComponent } from './components/tip-calculator/tip-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipCalculatorComponent
   ],
   imports: [
     BrowserModule,
